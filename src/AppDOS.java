7878b5e483f674f545d12a777c068296c475a536
LucasGCardoso <lucasgavirachicc@gmail.com>
2020-05-24 17:30:41 -0300

    Adding basic solution with cases

--- src/AppDOS.java
+++ src/AppDOS.java
@@ -6,9 +6,10 @@ public class AppDOS
 {
     public static void main(String args[])
     {
-        Graph g = new Graph(120000); // +- 119 mil nomes diferentes
 
-        In arq = new In("caso1_cohen.txt");
+        String arquivo = "caso4_cohen.txt";
+
+        In arq = new In(arquivo);
         String aux = "";
 
         int tamanhoVertical = 1;
@@ -18,35 +19,107 @@ public class AppDOS
             tamanhoVertical++;
         }
 
-        char [][] labirinto = new char[tamanhoHorizontal][tamanhoVertical];
-
         arq.close();
 
-        In arq2 = new In("caso1_cohen.txt");
+        int localHeroiX = 0;
+        int localHeroiY = 0;
 
-        int j = 0;
+        int localVilaoX = 0;
+        int localVilaoY = 0;
 
+        int [][] labirintoMatriz = new int[tamanhoVertical][tamanhoHorizontal];
+        int y = 0;
+        In arq2 = new In(arquivo);
+
+        /*
+        * 1 - A
+        * 2 - B
+        * A partir do 4 teremos os pontos transformados em números
+        * 0 - #
+        * */
+
+        int vertice = 4;
         while((aux = arq2.readLine()) != null){
 
-            for (int i=0; i<tamanhoHorizontal; i++){
+            for (int x=0; x<tamanhoHorizontal; x++){
 
-                labirinto [j][i] = aux.charAt(i);
+                if(aux.charAt(x) == '.'){
+                    labirintoMatriz [y][x] = vertice;
+                    vertice++;
+                }else if(aux.charAt(x) == 'A'){
+                    labirintoMatriz [y][x] = 1;
+                    localHeroiX = x;
+                    localHeroiY = y;
 
+                }else if(aux.charAt(x) == 'B'){
+                    labirintoMatriz [y][x] = 2;
+                    localVilaoX = x;
+                    localVilaoY = y;
+                }
             }
 
-            j++;
+            y++;
         }
-
         arq2.close();
 
-        for (int i=0; i < tamanhoHorizontal; i++){
+        System.out.println("O heroi está em X: " + localHeroiX + " Y: " + localHeroiY);
+        System.out.println("O vilão está em X: " + localVilaoX + " Y: " + localVilaoY);
 
-            for (int k=0; k < tamanhoVertical; k++){
+        Graph grafoLabirinto = new Graph(tamanhoHorizontal * tamanhoVertical);
 
-                System.out.print(labirinto[i][k]);
+        for (int i=0; i < tamanhoVertical; i++){
+
+            for (int k=0; k < tamanhoHorizontal; k++){
+
+                if(labirintoMatriz[i][k] != 0){
+                    if(labirintoMatriz[i+1][k] != 0){
+                        grafoLabirinto.addEdge(labirintoMatriz[i][k], labirintoMatriz[i+1][k]);
+                    }
+                    if(labirintoMatriz[i][k+1] != 0){
+                        grafoLabirinto.addEdge(labirintoMatriz[i][k], labirintoMatriz[i][k+1]);
+                    }
+                }
 
             }
+        }
 
+        // Agora achamos o caminho mais curto.
+
+        BreadthFirstPaths caminhamentoLargura = new BreadthFirstPaths(grafoLabirinto, 1);
+        int tamanhoMenorCaminho = caminhamentoLargura.distTo(2);
+        System.out.println(tamanhoMenorCaminho);
+        Iterable<Integer> path = caminhamentoLargura.pathTo(2);
+
+        for (int vertex : path){
+            for(int i=0; i<tamanhoVertical; i++){
+                for(int k=0; k<tamanhoHorizontal; k++){
+                    if(labirintoMatriz[i][k] == vertex){
+                        labirintoMatriz[i][k] = 3;
+                    }
+                }
+            }
+        }
+        char [][] labirintoFinal = new char[tamanhoVertical][tamanhoHorizontal];
+
+        for(int i=0; i<tamanhoVertical; i++){
+            for(int k=0; k<tamanhoHorizontal; k++){
+
+                if(labirintoMatriz[i][k] == 3){
+                    labirintoFinal[i][k] = 'X';
+                }else if(labirintoMatriz[i][k] == 0){
+                    labirintoFinal[i][k] = '#';
+                }else{
+                    labirintoFinal[i][k] = '.';
+                }
+            }
+        }
+        labirintoFinal[localHeroiY][localHeroiX] = 'A';
+        labirintoFinal[localVilaoY][localVilaoX] = 'B';
+
+        for(int i=0; i<tamanhoVertical; i++){
+            for(int k=0; k<tamanhoHorizontal; k++){
+                System.out.print(labirintoFinal[i][k]);
+            }
             System.out.println("");
         }
 
